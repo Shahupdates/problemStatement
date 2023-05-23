951a0fb2e51a5c5af4774f46d4bd4719d76ff554
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-23 19:35:22 -0400

    Update ProductTypeValidator.java

--- src/main/java/org/lilyai/ProductTypeValidator.java
+++ src/main/java/org/lilyai/ProductTypeValidator.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class ProductTypeValidator {
     private static final Map<Integer, String> productTypes = new HashMap<>();
 
-    public static void setProductTypes(List<String[]> productTypeData) {
-        for (String[] row : productTypeData) {
-            int id = Integer.parseInt(row[0].replaceAll("\"", ""));
-            String productType = row[1];
-            productTypes.put(id, productType);
-        }
+public static void setProductTypes(List<String[]> productTypeData) {
+    for (int i = 1; i < productTypeData.size(); i++) {
+        String[] row = productTypeData.get(i);
+        int id = Integer.parseInt(row[0].replaceAll("\"", ""));
+        String productType = row[1];
+        productTypes.put(id, productType);
     }
-
+}
     public static boolean validate(String productType) {
         // Compare with product types in the database and return validation result
         for (String dbProductType : productTypes.values()) {
