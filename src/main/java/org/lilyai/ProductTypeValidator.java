8bd9460d6372c54efed471ef31434bbef19b6c4b
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-23 18:02:30 -0400

    Update ProductTypeValidator.java

--- src/main/java/org/lilyai/ProductTypeValidator.java
+++ src/main/java/org/lilyai/ProductTypeValidator.java
@@ -1,10 +1,29 @@
 package org.lilyai;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProductTypeValidator {
+    private static final Map<Integer, String> productTypes = new HashMap<>();
+
+    static {
+        // Add product type mappings from the database
+        productTypes.put(9, "Gifting Accessories");
+        productTypes.put(27, "Unknown Product Type");
+        productTypes.put(29, "Sets");
+        productTypes.put(56, "Equipment Gear");
+        productTypes.put(65, "Unprocessed");
+        // Add more product types...
+    }
+
     public static boolean validate(String productType) {
         // Compare with product types in the database and return validation result
-        // Implement the necessary database operations to validate the product type
-
-        return /* validation result */;
+        for (String dbProductType : productTypes.values()) {
+            if (dbProductType.equalsIgnoreCase(productType)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
+
