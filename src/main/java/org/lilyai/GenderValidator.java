4c3c01191ea69ab88fe62be8832b5e62d031498c
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-23 18:38:37 -0400

    Update GenderValidator.java

--- src/main/java/org/lilyai/GenderValidator.java
+++ src/main/java/org/lilyai/GenderValidator.java
@@ -8,12 +8,14 @@ public class GenderValidator {
     private static final Map<Integer, String> genders = new HashMap<>();
 
     public static void setGenders(List<String[]> genderData) {
-        for (String[] row : genderData) {
+        for (int i = 1; i < genderData.size(); i++) { // Start from index 1 to skip the header row
+            String[] row = genderData.get(i);
             int id = Integer.parseInt(row[0]);
             String gender = row[1];
             genders.put(id, gender);
         }
     }
+
 
     public static boolean validate(String gender) {
         // Compare with genders in the database and return validation result
