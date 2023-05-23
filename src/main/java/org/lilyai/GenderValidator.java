cdce858c648cfe0accb10ed7cf03e62f8d4455a1
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-23 18:05:33 -0400

    Update GenderValidator.java
    remove directly implanted gender data

--- src/main/java/org/lilyai/GenderValidator.java
+++ src/main/java/org/lilyai/GenderValidator.java
@@ -2,21 +2,17 @@ package org.lilyai;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 public class GenderValidator {
     private static final Map<Integer, String> genders = new HashMap<>();
 
-    static {
-        // Add gender mappings from the database
-        genders.put(0, "Women");
-        genders.put(1, "Men");
-        genders.put(2, "Boys");
-        genders.put(3, "Girls");
-        genders.put(4, "Unknown Person Type");
-        genders.put(5, "Unisex");
-        genders.put(10, "Infant Boys");
-        genders.put(11, "Infant Girls");
-        genders.put(12, "Not Applicable");
+    public static void setGenders(List<String[]> genderData) {
+        for (String[] row : genderData) {
+            int id = Integer.parseInt(row[0]);
+            String gender = row[1];
+            genders.put(id, gender);
+        }
     }
 
     public static boolean validate(String gender) {
