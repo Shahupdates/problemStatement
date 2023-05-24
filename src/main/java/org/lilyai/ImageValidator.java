6206d11783ee311d7e8524097fb01454425354d6
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-23 21:06:10 -0400

    Update ImageValidator.java
    Simplified image validation

--- src/main/java/org/lilyai/ImageValidator.java
+++ src/main/java/org/lilyai/ImageValidator.java
@@ -3,45 +3,28 @@ package org.lilyai;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ImageValidator {
-    private static final String IMAGE_DELIMITER = "\\|\\|";
+public class ImageValidationTask implements Callable<Void> {
+    private static final Logger LOGGER = Logger.getLogger(ImageValidationTask.class.getName());
+    private final String imageUrl;
+    private final String[] row;
 
-    public static boolean validate(String images) {
-        // Split images using the delimiter
-        String[] imageUrls = images.split(IMAGE_DELIMITER);
-
-        // Validate each image URL
-        for (String imageUrl : imageUrls) {
-            boolean imageValid = validateImage(imageUrl);
-
-            // If any image URL is invalid, return false
-            if (!imageValid) {
-                return false;
-            }
-        }
-
-        return true;
+    public ImageValidationTask(String imageUrl, String[] row) {
+        this.imageUrl = imageUrl;
+        this.row = row;
     }
 
-    private static boolean validateImage(String imageUrl) {
-        try {
-            // Check if the URL has a valid protocol
-            if (!imageUrl.matches("^(http|https)://.*$")) {
-                System.out.println("Invalid URL: " + imageUrl);
-                return false;
-            }
-
-            URL url = new URL(imageUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("HEAD");
-            int responseCode = connection.getResponseCode();
-            System.out.println("URL: " + imageUrl + ", Response Code: " + responseCode);
-            return responseCode == HttpURLConnection.HTTP_OK;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
+    @Override
+    public Void call() {
+        LOGGER.log(Level.INFO, "Validating image URL: {0}", imageUrl);
+        if (!ValidationUtils.isValidImageUrl(imageUrl)) {
+            LOGGER.log(Level.INFO, "Invalid Image URL: {0} for row: {1}", new Object[]{imageUrl, String.join(",", row)});
+        } else {
+            LOGGER.log(Level.INFO, "Valid Image URL: {0}", imageUrl);
         }
+        return null;
     }
-
 }
