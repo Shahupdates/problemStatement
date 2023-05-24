aab2ae7996104ff0936c12e1b94ecf2fae93590a
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-05-23 21:05:20 -0400

    Update CSVWriter.java
    Final edit  + simplification

--- src/main/java/org/lilyai/CSVWriter.java
+++ src/main/java/org/lilyai/CSVWriter.java
@@ -3,21 +3,26 @@ package org.lilyai;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CSVWriter {
-    public static void write(String filename, List<String[]> data) {
-        try (FileWriter writer = new FileWriter(filename)) {
-            // Check if the status file exists, create it if necessary
-            File file = new File(filename);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
+    private static final Logger LOGGER = Logger.getLogger(CSVWriter.class.getName());
 
-            for (String[] row : data) {
-                writer.write(String.join(",", row) + "\n");
+    public static void writeStatusFile(List<String[]> rows, String outputFile) {
+        try (FileWriter writer = new FileWriter(outputFile)) {
+            // Write header
+            writer.append("id,display_name,status\n");
+
+            for (String[] row : rows) {
+                // Get the status for the row
+                String status = ValidationUtils.determineRowStatus(row);
+
+                // Write row with status
+                writer.append(String.join(",", row) + "," + status + "\n");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Failed to write status file", e);
         }
     }
 }
